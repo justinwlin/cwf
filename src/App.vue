d0bbaeb312b262a3ab9b636b5318e5778899275e
Justin Lin <justinlin@Justins-MacBook-Pro-5.local>
2020-10-11 01:09:49 -0700

    Adding on change to call api request

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,50 @@
     <h1>OTHER PAGES</h1>
     <h1>INPUT - UPLOAD A FILE</h1>
     <h1>SHOW THE PARSED RESULTS</h1>
-    <HelloWorld></HelloWorld>
+    <input
+      ref="upload"
+      type="file"
+      name="file-upload"
+      @change="onUploadFiles"
+    />
   </div>
 </template>
 
 <script>
 import json from "./testFiles/exampleResponse.json";
-import HelloWorld from "./components/HelloWorld";
+
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
   data: function () {
     return {
       exresponse: json,
     };
+  },
+  methods: {
+    onUploadFiles() {
+      console.log(this.$refs.upload.files[0]);
+      var formdata = new FormData();
+      formdata.append(
+        "resume",
+        this.$refs.upload.files[0],
+        this.$refs.upload.files[0].name
+      );
+
+      var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+        mode: "cors",
+      };
+
+      let proxyCORS = "https://cors-anywhere.herokuapp.com/";
+      let targetURL = "https://jobs.lever.co/parseResume";
+      fetch(proxyCORS + targetURL, requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    },
   },
 };
 </script>
